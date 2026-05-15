144964ab1ef61de327404ec1c57436abe1da7e2b
Cristian Felipe Escobar Ordoñez <cristianfelipeescobar673@gmail.com>
2026-05-15 17:39:57 -0500

    proyecto Gsap

---
+++ src/constants/index.ts
@@ -0,0 +1,90 @@
+export const flavorList: IFlavorList[] = [
+    {
+        name: "Leche con Chocolate",
+        color: "brown",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Leche de Fresa",
+        color: "red",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Cookies & Cream",
+        color: "blue",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Chocolate con Crema de Cacahuate",
+        color: "orange",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Malteada de Vainilla",
+        color: "white",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Max Chocolate Milk",
+        color: "black",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+];
+
+export const nutrientList: INutrientList[] = [
+    { label: "Potasio", amount: "245mg" },
+    { label: "Calcio", amount: "500mg" },
+    { label: "Vitamina A", amount: "176mcg" },
+    { label: "Vitamina D", amount: "5mcg" },
+    { label: "Hierro", amount: "1mg" },
+];
+
+export const card: ICard[] = [
+    {
+        src: "/videos/review1.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Madison",
+        img: "/images/p1.webp",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/review2.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Alexander",
+        img: "/images/p2.webp",
+    },
+    {
+        src: "/videos/review3.mp4",
+        rotation: "rotate-z-[-4deg]",
+        name: "Andrew",
+        img: "/images/p3.webp",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/review4.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Bryan",
+        img: "/images/p4.webp",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/review5.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Chris",
+        img: "/images/p5.webp",
+    },
+    {
+        src: "/videos/review6.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Devante",
+        img: "/images/p6.webp",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/review7.mp4",
+        rotation: "rotate-z-[-3deg]",
+        name: "Melisa",
+        img: "/images/p7.webp",
+        translation: "translate-y-[10%]",
+    },
+];
---
+++ src/sections/FooterSection.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
+
+const FooterSection = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
+
+  return (
+    <footer className="footer-section">
+      <Image src="/images/footer-dip.webp" alt="" width={3000} height={3000} className="w-full object-cover -translate-y-1" />
+
+      <div className="2xl:h-[110dvh] relative md:pt-[20vh] pt-[10vh]">
+        <div className="overflow-hidden z-10">
+          <h1 className="general-title text-center text-milk py-5">#BEBERESPONSABLEMENTE</h1>
+        </div>
+
+        {isMobile ? (
+          <Image src="/images/footer-drink.webp" alt="" width={3000} height={3000} className="absolute top-0 object-contain" />
+        ) : (
+          <video src="/videos/splash1.mp4" autoPlay playsInline muted className="absolute top-0 object-contain mix-blend-lighten" />
+        )}
+
+        <div className="flex-center gap-5 relative z-10 md:mt-20 mt-5">
+          <div className="social-btn">
+            <Image src="./images/yt.svg" alt="" width={50} height={50} />
+          </div>
+          <div className="social-btn">
+            <Image src="./images/insta.svg" alt="" width={50} height={50} />
+          </div>
+          <div className="social-btn">
+            <Image src="./images/tiktok.svg" alt="" width={50} height={50} />
+          </div>
+        </div>
+
+        <div className="mt-40 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
+          <div className="flex items-center md:gap-16 gap-5">
+            <div>
+              <p>Sabores SPYLT</p>
+            </div>
+            <div>
+              <p>Club del Trago</p>
+              <p>Marketing Estudiantil</p>
+              <p>Distribuidores Lácteos</p>
+            </div>
+            <div>
+              <p>Empresa</p>
+              <p>Contactos</p>
+              <p>Charla Sabrosa</p>
+            </div>
+          </div>
+
+          <div className="md:max-w-lg">
+            <p>¡Obtén acceso anticipado exclusivo y mantente informado sobre actualizaciones de productos, eventos y más!</p>
+            <div className="flex justify-between items-center border-b border-[#D9D9D9] py-5 md:mt-10">
+              <input type="email" placeholder="Introduce tu correo electrónico" className="w-full placeholder:font-sans placeholder:text-[#999999]" />
+              <Image src="/images/arrow.svg" alt="arrow" width={70} height={70} />
+            </div>
+          </div>
+        </div>
+
+        <div className="copyright-box">
+          <p>
+            Clonado por <a href="https://ahmedragab.netlify.app" target="_blank">Ahmed Ragab</a> - Copyright © 2025 Spylt - Todos los derechos reservados
+          </p>
+          <div className="flex items-center gap-7">
+            <p>Política de Privacidad</p>
+            <p>Términos de Servicio</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterSection;
