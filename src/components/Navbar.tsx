088e9e92a674dbd49a9ef2787b15415069f76416
Cristian Felipe Escobar Ordoñez <cristianfelipeescobar673@gmail.com>
2026-05-15 18:18:13 -0500

    mejoras visuales en desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 z-50 p-3 md:p-9">
+    <nav className="fixed top-0 left-0 z-50 p-6 md:p-9">
       <a href="/" className="cursor-pointer">
         <Image width={80} height={80} src="/images/nav-logo.svg" alt="nav-logo" className="w-20 md:w-24" />
       </a>
